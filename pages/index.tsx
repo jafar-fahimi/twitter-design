42587b06ef3ece1d8a7fae223bebf4cf01ad9951
jafar <jafar.fahimi12@gmail.com>
2023-03-05 18:15:28 +0430

    _document.tsx added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Head>
+        <title>Twitter Design</title>
+        <link rel="icon" href="twitter.svg" />
+      </Head>
+      <div>Home</div>
+    </main>
+  );
 }
