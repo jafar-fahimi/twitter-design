95c3468a6adfa3ce836300615aab096badf75335
jafar <jafar.fahimi12@gmail.com>
2023-03-06 16:11:49 +0430

    Input.tsx filePickerRef added

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import { PhotographIcon, XIcon } from "@heroicons/react/outline";
+import React, { useRef, useState } from "react";
 
 export default function Input() {
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef = useRef<any>(null);
 
+  // divide-y added border top & bottom for each elem in container.
   return (
     <div
       className={`flex space-x-3 overflow-y-scroll border-b border-gray-700 p-3 scrollbar-hide ${
@@ -25,7 +29,36 @@ export default function Input() {
             rows={2}
             className="min-h-[50px] w-full bg-transparent text-lg tracking-wide text-[#d9d9d9] placeholder-gray-500 outline-none"
           />
+
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute top-1 left-1 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-[#15181c] bg-opacity-75 hover:bg-[#272c26]"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="h-5 text-white" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="max-h-80 rounded-2xl object-contain"
+              />
+            </div>
+          )}
         </div>
+        {!loading && (
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center">
+              <div
+                className="icon"
+                onClick={() => filePickerRef.current?.click()}
+              >
+                <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+                <input type="file" ref={filePickerRef} hidden />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
