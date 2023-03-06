2e2f2156c7d212fed423d2dcb94595915ba8be32
jafar <jafar.fahimi12@gmail.com>
2023-03-06 14:25:18 +0430

    SidebarLink.tsx coded

--- components/SidebarLink.tsx
+++ components/SidebarLink.tsx
@@ -1,11 +1,25 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {
   text: string;
-  Icon: null | string;
-  active?:'';
+  Icon: any;
+  active?: "";
 };
 
-export default function SidebarLink({}: Props) {
-  return <section>SidebarLink</section>;
+function SidebarLink({ Icon, text, active }: Props) {
+  const router = useRouter();
+  return (
+    <div
+      className={`hoverAnimation flex items-center justify-center space-x-3 text-xl text-[#d9d9d9] xl:justify-start ${
+        active && "font-bold"
+      }`}
+      onClick={() => active && router.push("/")}
+    >
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </div>
+  );
 }
+
+export default SidebarLink;
