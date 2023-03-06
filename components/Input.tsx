b7b5fcf30e0666d85ba964ed217baf5260969a91
jafar <jafar.fahimi12@gmail.com>
2023-03-06 16:30:10 +0430

    input.tsx Tweet btn & sendPost coded

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,6 +6,7 @@ import {
   XIcon,
 } from "@heroicons/react/outline";
 import React, { useRef, useState } from "react";
+import { Picker } from "emoji-mart";
 
 export default function Input() {
   const [input, setInput] = useState("");
@@ -13,6 +14,16 @@ export default function Input() {
   const [selectedFile, setSelectedFile] = useState(null);
   const filePickerRef = useRef<any>(null);
   const [showEmojis, setShowEmojis] = useState(false);
+
+  const sendPost = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    setLoading(false);
+    setInput("");
+    setSelectedFile(null);
+    setShowEmojis(false);
+  };
 
   // divide-y added border top & bottom for each elem in container.
   return (
@@ -74,7 +85,27 @@ export default function Input() {
               <div className="icon">
                 <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
               </div>
+              {/* {showEmojis && (
+                <Picker
+                  onSelect={addEmoji}
+                  style={{
+                    position: "absolute",
+                    marginTop: "465px",
+                    marginLeft: -40,
+                    maxWidth: "320px",
+                    borderRadius: "20px",
+                  }}
+                  theme="dark"
+                />
+              )} */}
             </div>
+            <button
+              className="rounded-full bg-[#1d9bf0] px-4 py-1.5 font-bold text-white shadow-md hover:bg-[#1a8cd8] disabled:cursor-default disabled:opacity-50 disabled:hover:bg-[#1d9bf0]"
+              disabled={!input && !selectedFile}
+              onClick={sendPost}
+            >
+              Tweet
+            </button>
           </div>
         )}
       </div>
