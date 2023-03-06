1055791e586ff5f09340a37fc00ef885888365fb
jafar <jafar.fahimi12@gmail.com>
2023-03-06 14:25:11 +0430

    initial Sidebar.tsx coded

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import SidebarLink from "./SidebarLink";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
 
 export default function Sidebar() {
-  return <section>Sidebar</section>;
+  return (
+    <section>
+      <div>
+        <div className="mt-4 mb-2.5 space-y-2.5 xl:ml-24">
+          <SidebarLink text="Home" Icon={HomeIcon} />
+          <SidebarLink text="Explore" Icon={HashtagIcon} />
+          <SidebarLink text="Notifications" Icon={BellIcon} />
+          <SidebarLink text="Messages" Icon={InboxIcon} />
+          <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+          <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+          <SidebarLink text="Profile" Icon={UserIcon} />
+          <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+        </div>
+      </div>
+    </section>
+  );
 }
