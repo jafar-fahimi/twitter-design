dd1cd3db975532770a38aa430b1b147e25b684c0
jafar <jafar.fahimi12@gmail.com>
2023-03-09 09:46:59 +0430

    Sidebar.tsx take session from useSession & use

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,54 +1,57 @@
-import React from "react";
-import SidebarLink from "./SidebarLink";
-import { HomeIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/24/solid";
 import {
   HashtagIcon,
   BellIcon,
   InboxIcon,
   BookmarkIcon,
-  ClipboardListIcon,
   UserIcon,
-  DotsCircleHorizontalIcon,
-  DotsHorizontalIcon,
-} from "@heroicons/react/outline";
-import Image from "next/image";
+  ClipboardDocumentListIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/24/outline";
 
-export default function Sidebar() {
+import SidebarLink from "./SidebarLink";
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+function Sidebar() {
+  const { data: session } = useSession();
+
   return (
-    <section className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
-      <div className="hoverAnimation flex h-14 w-14 items-center justify-center p-0 xl:ml-24">
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center p-1 hoverAnimation xl:ml-24">
         <Image alt="Twitter Logo" src="twitter.svg" width={30} height={30} />
       </div>
-      <div className="mt-4 mb-2.5 space-y-2.5 xl:ml-24">
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
         <SidebarLink text="Home" Icon={HomeIcon} />
         <SidebarLink text="Explore" Icon={HashtagIcon} />
         <SidebarLink text="Notifications" Icon={BellIcon} />
         <SidebarLink text="Messages" Icon={InboxIcon} />
         <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardDocumentListIcon} />
         <SidebarLink text="Profile" Icon={UserIcon} />
-        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+        <SidebarLink text="More" Icon={EllipsisHorizontalIcon} />
       </div>
-      <button className="ml-auto hidden h-[52px] w-56 rounded-full bg-[#1d9bf0] text-lg font-bold text-white shadow-md hover:bg-[#1a8cd8] xl:inline">
+      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold py-3 shadow-md hover:bg-[#1a8cd8]">
         Tweet
       </button>
       <div
-        className="hoverAnimation mt-auto flex w-4/5 items-center justify-between text-[#d9d9d9] xl:ml-auto xl:-mr-5"
-        onClick={() => {}}
+        onClick={() => signOut}
+        className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5"
       >
-        <div className="ml-2 flex">
-          <img
-            src="images/people(1).png"
-            alt=""
-            className="h-10 w-10 rounded-full xl:mr-2.5"
-          />
-          <div className="hidden leading-5 xl:inline">
-            <h4 className="font-bold">text</h4>
-            <p className="text-[#6e767d]">@text</p>
-          </div>
+        <img
+          src={session?.user?.image || "people(1).png"}
+          alt="User Image"
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden xl:inline leading-5 w-28">
+          <h4 className="font-bold">{session?.user?.name || "name"}</h4>
+          <p className="text-[#6e767d]">@{session?.user?.tag}</p>
         </div>
-        <DotsHorizontalIcon className="ml-10 hidden h-5 xl:inline" />
+        <EllipsisHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
-    </section>
+    </div>
   );
 }
+
+export default Sidebar;
