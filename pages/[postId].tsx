6f74d57c9e1107cbe28721cc6fda2c359c2443ea
jafar <jafar.fahimi12@gmail.com>
2023-03-13 21:54:03 +0430

    title element received an array bug fixed [postId]

--- pages/[postId].tsx
+++ pages/[postId].tsx
@@ -37,7 +37,7 @@ const PostPage: NextPage<any> = ({
   const { data: session } = useSession();
 
   // Warning: A title element received an array with more than 1 element as children. In browsers title Elements can only have Text Nodes as children. If the children being rendered output more than a single text node in aggregate the browser will display markup and comments as text in the title and hydration will likely fail and fall back to client rendering
-  const titleText = `${post?.username} on Twitter: ${post?.text}`;
+  const titleText = `${post?.username} on Twitter: "${post?.text}"`;
 
   useEffect(
     () =>
@@ -64,9 +64,7 @@ const PostPage: NextPage<any> = ({
   return (
     <div>
       <Head>
-        <title>
-          {post?.username} on Twitter: "{post?.text}"
-        </title>
+        <title>{titleText}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
