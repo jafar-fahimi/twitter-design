6070de901a291b429711b041150400dce689cc79
jafar <jafar.fahimi12@gmail.com>
2023-03-13 20:57:02 +0430

    Widgets.tsx initial level coded

---
+++ components/Widgets.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+const Widgets: FunctionComponent<any> = (trendingResults, followResults) => {
+  return <div>Widgets</div>;
+};
+
+export default Widgets;
