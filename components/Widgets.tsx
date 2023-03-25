d17ca18872eaf66a023ddae9ca5269310b211457
jafar <jafar.fahimi12@gmail.com>
2023-03-25 20:31:23 +0430

    Widgets.tsx; fix currentPathName !== "Explore"

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,7 +1,11 @@
 import Trending from "./Trending";
 import Image from "next/image";
 import { FunctionComponent } from "react";
-import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassCircleIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
 
 type PropsType = {
   trendingResults: {
@@ -23,33 +27,43 @@ const Widgets: FunctionComponent<PropsType> = ({
   trendingResults,
   followResults,
 }) => {
+  const router = useRouter();
+  const currentPathName = router.asPath.slice(1); // e.g: Explore
+
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
-      <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
-        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
-          <MagnifyingGlassCircleIcon className="text-gray-500 h-5 z-50" />
-          <input
-            type="text"
-            className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full focus:border-[#1d9bf0] rounded-full focus:bg-black focus:shadow-lg"
-            placeholder="Search Twitter"
-          />
-        </div>
-      </div>
-
-      <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
-        <h4 className="font-bold text-xl px-4">Trends for you</h4>
-        {trendingResults.map(
-          (
-            result: { tweetText: string; tweets: number; trendingText: string },
-            index: number
-          ) => (
-            <Trending key={index} result={result} />
-          )
-        )}
-        <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
-          Show more
-        </button>
-      </div>
+      {currentPathName !== "Explore" && ( // in Explore page trendingResults is shown in center & search above.
+        <>
+          <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+            <div className="flex items-center bg-[#202327] p-2 rounded-full relative">
+              <MagnifyingGlassIcon className="text-gray-500 h-6 p-0 z-50" />
+              <input
+                type="text"
+                className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full focus:border-[#1d9bf0] rounded-full focus:bg-black focus:shadow-lg"
+                placeholder="Search Twitter"
+              />
+            </div>
+          </div>
+          <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+            <h4 className="font-bold text-xl px-4">Trends for you</h4>
+            {trendingResults.map(
+              (
+                result: {
+                  tweetText: string;
+                  tweets: number;
+                  trendingText: string;
+                },
+                index: number
+              ) => (
+                <Trending key={index} result={result} />
+              )
+            )}
+            <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
+              Show more
+            </button>
+          </div>
+        </>
+      )}
 
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
--- pages/[postId].tsx
+++ pages/[postId].tsx
@@ -21,6 +21,8 @@ import { commentModalState } from "../atoms/atoms";
 import { db } from "../utils/firebase";
 import Comment from "../components/Comment";
 import { changedSessionType } from "../utils/typings";
+import Trending from "../components/Trending";
+import { KeyIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const PostPage: NextPage<any> = ({
   trendingResults = "",
@@ -74,17 +76,56 @@ const PostPage: NextPage<any> = ({
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-          <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
-            <div
-              className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
-              onClick={() => router.push("/")}
-            >
-              <ArrowLeftIcon className="h-5 text-white" />
-            </div>
-            Tweet
-          </div>
+          {postId !== "Explore" && (
+            <>
+              <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
+                <div
+                  className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
+                  onClick={() => router.push("/")}
+                >
+                  <ArrowLeftIcon className="h-5 text-white" />
+                </div>
+                Tweet
+              </div>
+              <Post id={postId as string} post={post} postPage />
+            </>
+          )}
 
-          <Post id={postId as string} post={post} postPage />
+          {postId === "Explore" && (
+            <>
+              <div className="sticky mx-4 top-0 py-1.5 bg-black z-50 w-10/12">
+                <div className="flex items-center justify-between bg-[#202327] p-2 rounded-full relative">
+                  <MagnifyingGlassIcon className="text-gray-500 h-6 p-0 z-50" />
+                  <input
+                    type="text"
+                    className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full focus:border-[#1d9bf0] rounded-full focus:bg-black focus:shadow-lg"
+                    placeholder="Search Twitter"
+                  />
+                  <KeyIcon className="h-6 text-gray-500 z-50 -mr-16" />
+                </div>
+              </div>
+
+              <div className="text-[#d9d9d9] space-y-3 pt-2 rounded-xl w-full">
+                <h4 className="font-bold text-xl px-4">Trends for you</h4>
+                {trendingResults.map(
+                  (
+                    result: {
+                      tweetText: string;
+                      tweets: number;
+                      trendingText: string;
+                    },
+                    index: number
+                  ) => (
+                    <Trending key={index} result={result} />
+                  )
+                )}
+                <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
+                  Show more
+                </button>
+              </div>
+            </>
+          )}
+
           {comments.length > 0 && (
             <div className="pb-72">
               {comments.map((comment: any) => (
@@ -112,16 +153,63 @@ export default PostPage;
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
-  let trendingResults = [];
-  let followResults = [];
+  let trendingResults: {
+    tweetText: string;
+    tweets: number;
+    trendingText: string;
+  }[] = [];
+  let followResults: {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    picture: string;
+  }[] = [];
 
   try {
-    trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then((res) =>
-      res.json()
-    );
-    followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then((res) =>
-      res.json()
-    );
+    trendingResults = [
+      { trendingText: "Trending", tweetText: "LinkedIn", tweets: 23000 },
+      {
+        trendingText: "Technology - Trending",
+        tweetText: "TypeScript",
+        tweets: 13000,
+      },
+      {
+        trendingText: "Politics - Trending",
+        tweetText: "Taliban",
+        tweets: 5400,
+      },
+      {
+        trendingText: "Culture - Trending",
+        tweetText: "New Year Eve",
+        tweets: 900,
+      },
+      {
+        trendingText: "Sport - trending",
+        tweetText: "Qatar",
+        tweets: 2300034,
+      },
+    ];
+
+    const fetchedData: {
+      data: {
+        id: string;
+        title: string;
+        firstName: string;
+        lastName: string;
+        picture: string;
+      }[];
+      total: number;
+      page: number;
+      limit: number;
+    } = await fetch(
+      process.env.NEXT_PUBLIC_DUMMYAPI_BASE_URL + "user?limit=5",
+      {
+        method: "GET",
+        headers: { "app-id": process.env.DUMMYAPI_TOKEN as string },
+      }
+    ).then((res) => res.json());
+    followResults = fetchedData.data;
   } catch (error) {
     if (error instanceof Error) console.log("error occured ", error.message);
   }
