09c0390419ead638d9800611dc56569f1ace3c2f
jafar <jafar.fahimi12@gmail.com>
2023-03-25 17:03:53 +0430

    Widgets.tsx followResults.map fixed

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -5,7 +5,13 @@ import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
 
 const Widgets: FunctionComponent<{
   trendingResults: [];
-  followResults: [];
+  followResults: {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    picture: string;
+  }[];
 }> = ({ trendingResults, followResults }) => {
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
@@ -32,29 +38,37 @@ const Widgets: FunctionComponent<{
 
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
-        {followResults.map((result: any, index: number) => (
-          <div
-            className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center"
-            key={index}
-          >
-            <Image
-              src={result.userImg}
-              width={50}
-              height={50}
-              alt="Image"
-              className="rounded-full"
-            />
-            <div className="ml-4 leading-5 group">
-              <h4 className="font-bold group-hover:underline">
-                {result.username}
-              </h4>
-              <h5 className="text-gray-500 text-[15px]">{result.tag}</h5>
+        {followResults.map(
+          (result: {
+            id: string;
+            title: string;
+            firstName: string;
+            lastName: string;
+            picture: string;
+          }) => (
+            <div
+              className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center"
+              key={result.id}
+            >
+              <Image
+                src={result.picture}
+                width={50}
+                height={50}
+                alt="Image"
+                className="rounded-full"
+              />
+              <div className="ml-4 leading-5 group">
+                <h4 className="font-bold group-hover:underline">
+                  {result.firstName} {result.lastName}
+                </h4>
+                <h5 className="text-gray-500 text-[15px]">{`@${result.firstName}${result.lastName}`}</h5>
+              </div>
+              <button className="ml-auto bg-white text-black rounded-full font-bold text-sm py-1.5 px-3.5">
+                Follow
+              </button>
             </div>
-            <button className="ml-auto bg-white text-black rounded-full font-bold text-sm py-1.5 px-3.5">
-              Follow
-            </button>
-          </div>
-        ))}
+          )
+        )}
         <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
           Show more
         </button>
