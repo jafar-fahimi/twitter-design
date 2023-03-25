d7aabd60f984c5bbc1aa265c06bceef8d8331190
jafar <jafar.fahimi12@gmail.com>
2023-03-25 17:57:40 +0430

    Trending.tsx style complted

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -8,14 +8,16 @@ type PropsType = {
 
 const Trending: FunctionComponent<PropsType> = ({ result }) => {
   return (
-    <div className="px-3 py-1 rounded-md">
-      <div className="hover:bg-white hover:bg-opacity-[0.03] py-1 cursor-pointer transition duration-200 ease-out flex items-center justify-between">
+    <div className="px-3 py-1 rounded-md hover:bg-gray-800/10">
+      <div className="py-1 cursor-pointer transition duration-200 ease-out flex items-center justify-between">
         <span className="text-gray-500">{result.trendingText}</span>
         <span className="p-1 rounded-full hover:bg-gray-800/90">
           <EllipsisHorizontalIcon className="h-5 text-gray-400 group-hover:text-[#1d9bf0]" />
         </span>
       </div>
-      <p className="font-bold">&#x23;{result.tweetText}</p>
+      <p className="font-bold hover:cursor-pointer hover:underline">
+        &#x23;{result.tweetText}
+      </p>
       <p className="text-gray-500">{result.tweets} Tweets</p>
     </div>
   );
