3bc64c324e7204f6f7976a2a5f7cb98befdfbd3b
jafar <jafar.fahimi12@gmail.com>
2023-03-25 17:53:35 +0430

    Widgets.tsx trendingResults.map changed

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 import { FunctionComponent } from "react";
 import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
 
-const Widgets: FunctionComponent<{
-  trendingResults: [];
+type PropsType = {
+  trendingResults: {
+    tweetText: string;
+    tweets: number;
+    trendingText: string;
+  }[];
+
   followResults: {
     id: string;
     title: string;
@@ -12,7 +17,12 @@ const Widgets: FunctionComponent<{
     lastName: string;
     picture: string;
   }[];
-}> = ({ trendingResults, followResults }) => {
+};
+
+const Widgets: FunctionComponent<PropsType> = ({
+  trendingResults,
+  followResults,
+}) => {
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
       <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
@@ -27,10 +37,15 @@ const Widgets: FunctionComponent<{
       </div>
 
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
-        <h4 className="font-bold text-xl px-4">What's happening</h4>
-        {trendingResults.map((result: any, index: number) => (
-          <Trending key={index} result={result} />
-        ))}
+        <h4 className="font-bold text-xl px-4">Trends for you</h4>
+        {trendingResults.map(
+          (
+            result: { tweetText: string; tweets: number; trendingText: string },
+            index: number
+          ) => (
+            <Trending key={index} result={result} />
+          )
+        )}
         <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
           Show more
         </button>
